0b7887d3d99b473708fd2bd6922e122132f07595
Marina Iurchenko <lamanch1203@gmail.com>
2023-06-24 16:13:11 +0300

    change test

--- src/test/java/ru/netology/orderCard/DebitCardTest.java
+++ src/test/java/ru/netology/orderCard/DebitCardTest.java
@@ -1,31 +1,31 @@
 package ru.netology.orderCard;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.openqa.selenium.By.*;
+
 
 public class DebitCardTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll(){
+    static void setUpAll() {
         WebDriverManager.chromedriver().setup();
-
 
 
     }
 
     @BeforeEach
-    public void setUp(){
+    void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -36,21 +36,23 @@ public class DebitCardTest {
     }
 
     @AfterEach
-    public void setUpAfter(){
+    void setUpAfter() {
         driver.quit();
         driver = null;
     }
 
     @Test
     void shouldTestOrderCard() throws InterruptedException {
-        open("http://localhost:9999/");
-        $(cssSelector("[data-test-id=name] input")).sendKeys("Кузнецов Анатолий Николаевич");
-        $(cssSelector("[data-test-id-phone] input")).sendKeys("+78673425109");
-        $(cssSelector("[data-test-id=agreement]")).click();
-        $(cssSelector("button")).click();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближаейшее время";
-        String actual = cssSelector("[data-test-id=name] .input_invalid .input__sub").findElement(driver).getText().trim();
-        assertEquals(expected, actual);
+        driver.get("http://localhost:9999/");
+
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Кузнецов Анатолий");
+        elements.get(1).sendKeys("+79012345678");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+
         Thread.sleep(5000);
 
 
